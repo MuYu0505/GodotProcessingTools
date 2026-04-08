df1982d03bc9424f36c6f3aad9affb040e24e988
weipeng.chen <weipeng.chen@yisurvye.com>
2026-04-08 18:35:22 +0800

    fix: 1.视频转帧功能Demo 2.修改导出像素

--- src/main/java/com/pixeltool/dto/ProcessOptions.java
+++ src/main/java/com/pixeltool/dto/ProcessOptions.java
@@ -1,5 +1,8 @@
 package com.pixeltool.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ProcessOptions {
 
     private boolean removeWatermark = true;
